de9be2d36534cf8267a068f0ce05f2c36bd3068a
Simon Mitternacht <simon.mitternacht@gmail.com>
2018-11-03 16:44:28 +0100

    Change header layout and style.

--- src/components/header.js
+++ src/components/header.js
@@ -15,22 +15,43 @@ const Header = ({ siteTitle }) => (
         padding: '1.45rem 1.0875rem',
       }}
     >
-        <div style={{ marginTop: '0', display: 'flex', justifyContent: 'flex-end', fontSize: '0.7rem' }}>
-            <div style={{ marginRight: '1rem' }}>
-                <Link to="/albums">Albums</Link>
+        <div style=
+            {{
+                display: 'flex',
+                justifyContent: 'space-between',
+                borderBottom: '2px solid black',
+                paddingBottom: '16px'
+            }}
+            className="header"
+        >
+            <div style={{ }}>
+                <h1 style={{ margin: 0 }} >
+                    <Link
+                      to="/"
+                      style={{ textDecoration: 'none' }}
+                    >
+                        {siteTitle}
+                    </Link>
+                </h1>
             </div>
-            <div>
-                <Link to="/concerts">Concerts</Link>
+            <div className="menu">
+                <div>
+                    <Link to="/albums">Albums</Link>
+                </div>
+                <div>
+                    <Link to="/concerts">Concerts</Link>
+                </div>
+                <div>
+                    <Link to="/videos">Videos</Link>
+                </div>
+                <div>
+                    <Link to="/press">Press</Link>
+                </div>
+                <div>
+                    <Link to="/contact">Contact</Link>
+                </div>
             </div>
         </div>
-        <h1 style={{ margin: 0, textAlign: 'center' }} >
-            <Link
-              to="/"
-              style={{ textDecoration: 'none' }}
-            >
-                {siteTitle}
-            </Link>
-        </h1>
     </div>
   </div>
 )
