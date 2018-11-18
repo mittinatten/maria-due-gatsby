512648302414d06697f817078d5dd1213ffb52cc
Simon Mitternacht <simon.mitternacht@gmail.com>
2018-11-18 20:05:42 +0100

    Add check to not fail on songs without albums.

--- src/templates/song.js
+++ src/templates/song.js
@@ -5,17 +5,25 @@ import Layout from "../components/layout";
 
 export default ({ data }) => {
     let lyrics;
+    let album;
+
     if (data.song.lyrics) {
         lyrics = data.song.lyrics.split('\n').map((line, index) =>
             <div key={index} style={{textIndent: '-1em', marginLeft: '1em', minHeight: '1em' }}>{ line }</div>);
+    }
+
+    if (data.song.album && data.song.fields && data.song.fields.albumSlug) {
+        album =
+            <p style={{marginTop: '2em'}}>
+                From <a href={data.song.fields.albumSlug}>{ data.song.album.title }</a> { data.song.album.year }
+            </p>
     }
 
     return (
         <Layout>
             <h2>{ data.song.title }</h2>
             { lyrics }
-            <p style={{marginTop: '2em'}}>
-                From <a href={data.song.fields.albumSlug}>{ data.song.album.title }</a> { data.song.album.year }</p>
+            { album }
         </Layout>
     )
 }
